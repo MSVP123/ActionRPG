5d7b5028e9ca57f1e65da0f5555abc9e940848cc
bluenite580 <bluenite580@penguin>
2021-12-21 18:31:58 -0800

    Player Animations

--- Action RPG Resources/Player/player.gd
+++ Action RPG Resources/Player/player.gd
@@ -5,6 +5,9 @@ const MAX_SPEED = 100
 const FRICTION = 500
 
 var velocity = Vector2.ZERO
+onready var animation_player = $AnimationPlayer
+onready var animation_tree = $AnimationTree
+onready var animation_state = $AnimationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -13,8 +16,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 
 	if input_vector != Vector2.ZERO:
+		animation_tree.set("parameters/Idle/blend_position", input_vector)
+		animation_tree.set("parameters/Run/blend_position", input_vector)
+		animation_state.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animation_state.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 	velocity = move_and_slide(velocity)
