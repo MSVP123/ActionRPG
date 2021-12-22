0a4dd8d836e97745b60dfe8254e8378e4797ec0e
bluenite580 <bluenite580@penguin>
2021-12-21 21:07:51 -0800

    Input Map, Attacking, State Machine

--- Action RPG Resources/Player/player.gd
+++ Action RPG Resources/Player/player.gd
@@ -5,19 +5,36 @@ const MAX_SPEED = 75
 const FRICTION = 500
 
 var velocity = Vector2.ZERO
+var state = MOVE
+enum { MOVE, ROLL, ATTACK
+}
+
 onready var animation_player = $AnimationPlayer
 onready var animation_tree = $AnimationTree
 onready var animation_state = $AnimationTree.get("parameters/playback")
 
+func _ready():
+	animation_tree.active = true
+
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+		ATTACK:
+			attack_state(delta)
+
+func move_state(delta):
 	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	input_vector.y = Input.get_action_strength("backwords") - Input.get_action_strength("forward")
 	input_vector = input_vector.normalized()
 
 	if input_vector != Vector2.ZERO:
 		animation_tree.set("parameters/Idle/blend_position", input_vector)
 		animation_tree.set("parameters/Run/blend_position", input_vector)
+		animation_tree.set("parameters/Attack/blend_position", input_vector)
 		animation_state.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
@@ -25,3 +42,14 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_pressed("attack"):
+		velocity = Vector2.ZERO
+		state = ATTACK
+
+
+func attack_state(delta):
+	animation_state.travel("Attack")
+
+func attack_animation_finished():
+	state = MOVE
