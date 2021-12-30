1dbc23a7358a503254fa59d5e80cf7991f003827
MSVP123 <bluenite580@gmail.com>
2021-12-30 11:05:41 -0800

    Hit effect

---
+++ Overlap/hurtBox.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+export var show_hit = true
+
+const HitEffect = preload("res://Effects/HitEffect/HitEffect.tscn")
+
+func _on_hurtBox_area_entered(area):
+	if show_hit:
+		var effect = HitEffect.instance()
+		var main  = get_tree().current_scene
+		main.add_child(effect)
+		effect.global_position = global_position
